0aaa1d7cfb6e00914ce623083e969b3dae3c578b
appdevworkscripts <nadeem.sajjad@zenways.io>
2017-09-04 12:50:13 +0530

    okok

---
+++ 4 Sept/contact_sample/js/app.js
@@ -0,0 +1,38 @@
+const app=angular.module('myapp',[]);
+app.controller('AController',function($scope,$http){
+    var refresh=function(){
+        $http({
+            url:'https://zenways-contact.herokuapp.com/api/contacts',
+            headers:{
+                key:'ZENWAYS01SAGAR'
+            }
+        }).then(function(response){
+            console.log(response.data);
+            $scope.contacts=response.data.contacts;
+        },function(response){
+            console.log(response);
+        });
+    }
+    refresh();
+
+    $scope.submitForm=function(){
+        $http({
+            url:'https://zenways-contact.herokuapp.com/api/contact',
+            headers:{
+                key:'ZENWAYS01SAGAR',
+                'content-type':'application/json'
+            },
+            data:JSON.stringify($scope.contact),
+            method:'POST'
+
+        }).then(function(response){
+            refresh();
+            $scope.contact={}
+            console.log(response);
+            alert('Successfully Added Contact')
+        },function(response){
+            console.log(response);
+            alert('Something went wrong');
+        });
+    }
+});
